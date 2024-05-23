5e8db5da6fccdd6b5a37680446b72596c2920c88
unknown <yoho1019@naver.com>
2024-05-23 23:34:39 +0900

    배열 1차문제 js만 품.

---
+++ 10807/helloworld.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+
+const input = fs.readFileSync("10807/test.txt").toString().split("\n");
+
+let count = 0;
+
+const secondInput = input[1];
+const threeInput = input[2];
+
+const loopState = secondInput.split(" ");
+
+
+for(var state  in loopState){
+    if(loopState[state] === threeInput){
+        count++;
+    }
+}
+
+console.log(count);
